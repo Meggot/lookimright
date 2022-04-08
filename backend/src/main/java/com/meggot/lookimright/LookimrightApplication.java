9c8ffd4cbd07522fd9ed2f2f6835b6cc2d3fe866
bradleywilliams <bradleywilliamsgb@gmail.com>
2022-04-08 11:37:15 +0100

    Backend - Initial Project Setup

---
+++ backend/src/main/java/com/meggot/lookimright/LookimrightApplication.java
@@ -0,0 +1,13 @@
+package com.meggot.lookimright;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LookimrightApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LookimrightApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/meggot/lookimright/LookimrightApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meggot.lookimright;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LookimrightApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
